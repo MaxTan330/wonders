f714641f0f2e6c9a995596b6d1eb40ae06f10814
tanpeng <tanpeng@wondersgroup.com>
2019-12-17 17:04:47 +0800

    '字体构建包'

--- packages/wonders-icons/src/main.js
+++ packages/wonders-icons/src/main.js
@@ -3,23 +3,43 @@
  * @Description: wonders-icon入口文件
  * @Date: 2019/12/16 17:13:52
  */
-const gulp = require('gulp');
-const iconfont = require('gulp-iconfont');
+const { series, src, dest } = require('gulp');
+const gulpClean = require('gulp-clean');
+const gulpWatch = require('gulp-watch');
+const gulpIconfont = require('gulp-iconfont');
 const iconfontCss = require('gulp-iconfont-css');
-const fontName = 'icons';
-gulp.task('default', function() {
-    gulp.src(['../assets/svgs/test.svg'])
+const fontName = 'wonders-icons';
+const watch = () => {
+    return gulpWatch('../assets/*', () => {
+        series(clean, move, iconfont); //执行任务
+    });
+};
+const clean = () => {
+    return src('../build', { read: false }).pipe(gulpClean({ force: true }));
+};
+const move = done => {
+    src('../assets/css/demo.css').pipe(dest('../build/css'));
+    src('../assets/index.html').pipe(dest('../build'));
+    done && done();
+};
+const iconfont = done => {
+    src('../assets/svgs/*.svg')
         .pipe(
             iconfontCss({
                 fontName: fontName,
-                path: '../assets/css/icon.scss',
-                fontPath: '../../fonts/icons/'
+                path: '../assets/css/template.scss',
+                fontPath: '../build/',
+                cssClass: 'wonders-icons',
+                targetPath: 'css/wonders-icons.css'
             })
         )
         .pipe(
-            iconfont({
+            gulpIconfont({
+                formats: ['svg', 'ttf', 'eot', 'woff', 'woff2'],
                 fontName: fontName
             })
         )
-        .pipe(gulp.dest('../assets/fonts/icons/'));
-});
+        .pipe(dest('../build'));
+    done && done();
+};
+exports.default = series(clean, move, iconfont, watch);
