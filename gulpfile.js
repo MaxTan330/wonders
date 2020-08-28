403296aee8eb56103df826553e3e6bd73acaacf6
maxtan <maxtan330@163.com>
2020-08-28 13:58:49 +0800

    nts-router包添加

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,10 @@ const base64Conf = {
     projectPath: './packages/base64',
     outPath: './packages/base64/lib',
 };
+const ntsRouterConf = {
+    projectPath: './packages/nts-router',
+    outPath: './packages/nts-router/lib',
+};
 //清空tohtml的lib目录
 const cleanTohtmlLib = () => {
     return src(tohtmlConf.outPath, { read: false, allowEmpty: true }).pipe(gulpClean({ force: true }));
@@ -49,13 +53,22 @@ const tsIconfontTask = () => {
 const cleanBase64Lib = () => {
     return src(base64Conf.outPath, { read: false, allowEmpty: true }).pipe(gulpClean({ force: true }));
 };
-//编译iconfont 项目ts文件
+//编译base64 项目ts文件
 const tsBase64Task = () => {
     return src(`${base64Conf.projectPath}/src/**/*.ts`).pipe(tsProject()).pipe(dest(base64Conf.outPath));
+};
+//清空nts-router的lib目录
+const cleanNtsRouterLib = () => {
+    return src(ntsRouterConf.outPath, { read: false, allowEmpty: true }).pipe(gulpClean({ force: true }));
+};
+//编译nts-router项目ts文件
+const tsNtsRouterTask = () => {
+    return src(`${ntsRouterConf.projectPath}/src/**/*.ts`).pipe(tsProject()).pipe(dest(ntsRouterConf.outPath));
 };
 const IconfontSingleTask = series(copyIconfont, tsIconfontTask);
 const TohtmlSingleTask = series(copyTohtml, tsTohtmlTask);
 const Base64SingleTask = series(tsBase64Task);
+const NtsRouterSingleTask = series(tsNtsRouterTask);
 //监听tohtml编译任务
 const watchBuildTohtml = () => {
     return gulpWatch(`${tohtmlConf.projectPath}/src/**/*`, TohtmlSingleTask);
@@ -68,18 +81,26 @@ const watchBuildIconfont = () => {
 const watchBuildBase64 = () => {
     return gulpWatch(`${base64Conf.projectPath}/src/**/*`, Base64SingleTask);
 };
+//监听nts-router编译任务
+const watchBuildNtsRouter = () => {
+    return gulpWatch(`${ntsRouterConf.projectPath}/src/**/*`, NtsRouterSingleTask);
+};
 //tohtml 开发任务
 const tohtmlDev = series(cleanTohtmlLib, TohtmlSingleTask, watchBuildTohtml);
 //iconfont 开发任务
 const iconfontDev = series(cleanIconfontLib, IconfontSingleTask, watchBuildIconfont);
 //base64 开发任务
 const base64Dev = series(cleanBase64Lib, Base64SingleTask, watchBuildBase64);
+//nts-router 开发任务
+const ntsRouterDev = series(cleanNtsRouterLib, NtsRouterSingleTask, watchBuildNtsRouter);
 //tohtml 构建任务
 const tohtmlBuild = series(cleanTohtmlLib, TohtmlSingleTask);
 //iconfont 构建任务
 const iconfontBuild = series(cleanIconfontLib, IconfontSingleTask);
 //base64 构建任务
 const base64Build = series(cleanBase64Lib, Base64SingleTask);
+//nts-router 构建任务
+const ntsRouterBuild = series(cleanNtsRouterLib, NtsRouterSingleTask);
 
 exports.tohtmlDev = tohtmlDev;
 exports.iconfontDev = iconfontDev;
@@ -87,3 +108,5 @@ exports.tohtmlBuild = tohtmlBuild;
 exports.iconfontBuild = iconfontBuild;
 exports.base64Dev = base64Dev;
 exports.base64Build = base64Build;
+exports.ntsRouterDev = ntsRouterDev;
+exports.ntsRouterBuild = ntsRouterBuild;
