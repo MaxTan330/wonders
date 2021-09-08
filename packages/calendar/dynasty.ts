99faf0401e80787eeb0f39d7a1a5a552b18a87fe
tanpeng <peng.tan0814@sinocare.com>
2021-09-08 17:36:35 +0800

    初始化日历项目

---
+++ packages/calendar/dynasty.ts
@@ -0,0 +1,12 @@
+/**
+ * @Author: MaxTan
+ * @Description: 朝代表
+ * @Date: 2021/09/08 17:09:41
+ */
+/**
+ * 朝代对照类
+ */
+class Dynasty {
+  private dynastyDic: string;
+  constructor() {}
+}
---
+++ packages/calendar/index.ts
@@ -0,0 +1,5 @@
+/**
+ * @Author: MaxTan
+ * @Description: 日历入口文件
+ * @Date: 2021/09/08 17:07:02
+ */
